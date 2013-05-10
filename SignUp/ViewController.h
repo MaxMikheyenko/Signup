3cb2b5235e9801e5c9507e4849f04ba181d232a6
Max Mikheyenko <max.mikheyenko@gmail.com>
2013-05-10 18:57:37 -0400

    Initial Commit

---
+++ SignUp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SignUp
+//
+//  Created by Max Mikheyenko on 5/10/13.
+//  Copyright (c) 2013 Max Mikheyenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SignUp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SignUp
+//
+//  Created by Max Mikheyenko on 5/10/13.
+//  Copyright (c) 2013 Max Mikheyenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
